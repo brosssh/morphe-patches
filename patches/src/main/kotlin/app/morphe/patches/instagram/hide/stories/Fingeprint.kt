48b29fddedaa8cfb0192407cf69263ef1374791e
brosssh <tiabroch@gmail.com>
2026-03-20 12:02:01 +0100

    chore: Small fix

--- patches/src/main/kotlin/app/morphe/patches/instagram/hide/stories/Fingeprint.kt
+++ patches/src/main/kotlin/app/morphe/patches/instagram/hide/stories/Fingeprint.kt
@@ -1,16 +1,16 @@
 package app.morphe.patches.instagram.hide.stories
 
 import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.opcode
+import app.morphe.patcher.OpcodesFilter
 import com.android.tools.smali.dexlib2.Opcode
 
 internal val getOrCreateAvatarViewFingerprint = Fingerprint(
     parameters = emptyList(),
     returnType = "L",
     definingClass = "Lcom/instagram/reels/ui/views/reelavatar/RecyclerReelAvatarView;",
-    filters = listOf(
-        opcode(Opcode.INVOKE_VIRTUAL),
-        opcode(Opcode.IPUT_OBJECT),
-        opcode(Opcode.INVOKE_VIRTUAL) // Add View (Story)
+    filters = OpcodesFilter.opcodesToFilters(
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.IPUT_OBJECT,
+        Opcode.INVOKE_VIRTUAL // Add View (Story)
     )
 )

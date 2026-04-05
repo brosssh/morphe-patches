9a7d21adb28de6bf923957450ea8dbbbc5407585
Florian Bernhart <70264417+fbernhart@users.noreply.github.com>
2026-04-05 11:53:51 +0200

    chore: Upgrade morphe-patcher to 1.3.3 & add extended Compatibility declarations (#99)

--- patches/src/main/kotlin/app/morphe/patches/mapy/misc/spoof/BypassSignatureCheck.kt
+++ patches/src/main/kotlin/app/morphe/patches/mapy/misc/spoof/BypassSignatureCheck.kt
@@ -1,5 +1,6 @@
 package app.morphe.patches.mapy.misc.spoof
 
+import app.morphe.patches.shared.Constants
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.patch.bytecodePatch
 
@@ -7,7 +8,7 @@ import app.morphe.patcher.patch.bytecodePatch
 val bypassSignatureCheck = bytecodePatch(
     name = "Bypass signature check",
 ) {
-    compatibleWith("cz.seznam.mapy")
+    compatibleWith(Constants.COMPATIBILITY_MAPY)
 
     execute {
         securityCheckFingerprint.method.addInstruction(0, "return-void")
